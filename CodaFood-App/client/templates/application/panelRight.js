e3c020276236f59c749c8a9bb85b766db8416a8c
J3m5 <anojems@gmail.com>
2017-09-21 17:00:05 +0200

    leftpanel amélioré

--- CodaFood-App/client/templates/application/panelRight.js
+++ CodaFood-App/client/templates/application/panelRight.js
@@ -3,49 +3,63 @@ Template.panelRight.helpers({
     cartitems: function(){
         return Cart.find({});
     },
+    total: function(){
+        const col = Cart.find();
+        col.forEach(function(el) {
+           return el.quantity * el.price;
+            })
 
+    }
 });
 
 
 
 Template.panelRight.events({
-    //add your events here
-    'click .modifycart'(event) {
-        // event.preventDefault();
-        let product = this.name;
-        let inc;
-        let op = event.target.textContent;
-        // let findOneProd = Cart.findOne({product: product});
-        if(op === "add"){
-            inc= 1;
-        } else if(op === "remove"){
-            inc= -1;
-        }
-        // if(findOneProd ){
-        if(Cart.findOne({product: product}).quantity === 1 && inc === -1){
-            Cart.remove({product: product})
-        }else{
-            Cart.update({product : product},
-                {
-                    $inc:{
-                        quantity: inc
+
+    'click .allin': function(event){
+        const col = Cart.find().fetch();
+        let checked = $('.chk').is(':checked');
+        col.forEach(function(el) {
+            Cart.update({product:el.product}, {
+                $set: {
+                    checked : !checked
+                }
+            })
+        });
+    },
+    'click .inc':function(event){
+        const col = Cart.find({checked:true}).fetch();
+        let inc = parseInt(event.currentTarget.id);
+        col.forEach(function(el) {
+            if(Cart.findOne({product: el.product}).quantity === 1 && inc === -1) {
+                Cart.remove({product: el.product})
+            } else {
+                Cart.update({product: el.product}, {
+                    $inc: {
+                        quantity: inc,
                     }
                 })
+            }
+        });
+        if(Cart.find().count() === 0) {
+            $( ".chk" ).prop( "checked", false );
         }
+
     }
 });
+
 Template.cart_list.helpers({
     total: function () {
         return this.price * this.quantity;
     },
     checked: function(){
         return Cart.findOne({product:this.product}).checked;
-    }
+    },
+
 });
 
 Template.cart_list.events({
     'click .product':function() {
-
         Cart.update({product: this.product},{
             $set:{
                 checked: !this.checked
